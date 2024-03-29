05578e299d8b957fdab498251a6f2d4905db1bd3
Sanny_okafor <okaforsan77@gmail.com>
2024-03-30 00:42:18 +0100

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeatherdata(response) {
+    let temperatureElement = document.querySelector("#temperature-data");
+    let temperature = response.data.temperature.current;
+    
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "08e18e31946af96bbbc5dt23b4o6f954";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=08e18e31946af96bbbc5dt23b4o6f954&units=metric';
+    axios.get(apiUrl).then(updateWeatherdata);
+}
+
+
 function submitSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", submitSearch );
+
+searchCity("London");
